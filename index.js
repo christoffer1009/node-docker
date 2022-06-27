3bac55e9a0bc8cd43661dc43ea46db973dda0418
Christoffer Cavalcante <christoffercavalcante@gmail.com>
2022-06-26 22:43:03 -0300

    adiciona nodemon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Docker!!!");
+  res.send("Hello from Docker!!!");
 });
 
 app.listen(3000);
